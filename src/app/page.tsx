96164770f2105502468f6ada69ece76e3663814b
Grellim <gre.jagua@gmail.com>
2025-09-10 02:36:58 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export function Home({ siteConfigData, locale, socialLinks, footerButtons, portf
       
       <main className="pt-16">
         {/* Hero Section with Video Background */}
-        <section id="home" className="relative h-[80] flex items-center justify-center pb-16">
+        <section id="home" className="relative h-80xl flex items-center justify-center pb-16">
           <VideoBackground
             videoSrc={siteConfigData.videoBackgroundUrl}
             fallbackImage="/videos/placeholder.jpg"
