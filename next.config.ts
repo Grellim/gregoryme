d833f653fd68eb91dfbed8ae51931897c84a78b7
Grellim <gre.jagua@gmail.com>
2025-09-11 19:15:28 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // 禁用 Next.js 热重载，由 nodemon 处理重编译
   reactStrictMode: false,
   webpack: (config, { dev }) => {
     if (dev) {
